24850baa14191ff1b4e2b159a83e98a31365c90b
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-07-08 09:31:57 +0300

    updated DOMContentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    const sentense = document.getElementById("text");
+    sentense.textContent = "This is really cool!";
+});
